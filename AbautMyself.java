835adfeac03008cd75285ad72396c3333491b6c4
sumeyye20 <71411837+sumeyye20@users.noreply.github.com>
2020-09-20 15:54:48 +0200

    Update AbautMyself.java

--- AbautMyself.java
+++ AbautMyself.java
@@ -4,7 +4,10 @@ public class AbautMyself {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+System.out.println(" my name is sümeyye," + "I am 30 years old,"+"I live in Ludwigsburg city"+ " I have two family member");
+System.out.println(" I am already loving java!!!");
 	}
-
 }
+		
